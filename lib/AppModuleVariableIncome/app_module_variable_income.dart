6eb6a3e94345f153c2be1afa6c7d9c683df45387
Acacio Egas <ajunior@guide.com.br>
2021-10-05 09:29:19 -0300

    V2

--- lib/AppModuleVariableIncome/app_module_variable_income.dart
+++ lib/AppModuleVariableIncome/app_module_variable_income.dart
@@ -2,5 +2,5 @@ import 'package:app_core/appcore/app_core.dart';
 
 /// A Calculator.
 class AppVariableIncome extends AppCore {
-  AppVariableIncome() : super(title: 'Guide', PageText: 'Renda Variavel');
+  AppVariableIncome() : super(title: 'Guide', PageText: 'Renda Variavel v2');
 }
